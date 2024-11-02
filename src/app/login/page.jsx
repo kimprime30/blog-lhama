0d30eaa69bd91cef32aa79954c08c5e5440b8c44
francisco aquino <kino.666@gmail.com>
2024-11-02 19:31:09 -0300

    configuracao do authjs

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,26 @@
+"use client";
+import { signIn, useSession } from "next-auth/react";
 import styles from "./loginpage.module.css";
+import { useRouter } from "next/navigation";
 
 const Loginpage = () => {
+  const { data, status } = useSession();
+
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+
+  if (status === "authenticated") {
+  }
+  
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton}>Login com o Google</div>
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
+          Login com o Google
+        </div>
         <div className={styles.socialButton}>Login com o Github</div>
         <div className={styles.socialButton}>Login com o Facebook</div>
       </div>
