0e52a738763cfb4b36c6527a509c9a7ffb99234e
francisco aquino <kino.666@gmail.com>
2024-11-05 19:58:34 -0300

    rota para post/slug, modificação singlepage, card e categorylist

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,9 +5,11 @@ import Link from "next/link";
 const Card = ({ key, item }) => {
   return (
     <div className={styles.container} key={key}>
-      <div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-      </div>
+      {item.img && (
+        <div className={styles.imageContainer}>
+          <Image src={item.img} alt="" fill className={styles.image} />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <div className={styles.detail}>
           <span className={styles.date}>
@@ -15,16 +17,11 @@ const Card = ({ key, item }) => {
           </span>
           <span className={styles.category}>{item.catSlug}</span>
         </div>
-        <Link href="/">
+        <Link href={`/posts/${item.slug}`}>
           <h1>{item.title}</h1>
         </Link>
-        <p className={styles.desc}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-          cupiditate facilis illo porro veniam alias laudantium rem quas soluta
-          saepe. Provident, minima beatae neque obcaecati ratione molestias
-          veritatis nam deserunt?
-        </p>
-        <Link href="/" className={styles.link}>
+        <p className={styles.desc}>{item.desc.substring(0, 60)}</p>
+        <Link href={`/posts/${item.slug}`} className={styles.link}>
           Leia mais
         </Link>
       </div>
