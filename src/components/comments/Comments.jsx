2329afe066da1d1147840697fcb7284410ceeb14
francisco aquino <kino.666@gmail.com>
2024-11-06 08:09:00 -0300

    configuracao da rota de criacao de comentarios

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,114 +1,83 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./comments.module.css";
 import Image from "next/image";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 
-const Comments = () => {
-  const status = "autenticated";
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    const error = new Error(data.message);
+    throw error;
+  }
+
+  return data;
+};
+
+const Comments = ({ postSlug }) => {
+  const { status } = useSession();
+
+  const { data, mutate, isLoading } = useSWR(
+    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    fetcher
+  );
+
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = async () => {
+    await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ desc, postSlug }),
+    });
+    mutate();
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Comments</h1>
-      {status === "autenticated" ? (
+      {status === "authenticated" ? (
         <div className={styles.write}>
           <textarea
-            placeholder="escreva seu comentário..."
+            placeholder="write a comment..."
             className={styles.input}
+            onChange={(e) => setDesc(e.target.value)}
           />
-          <button className={styles.button}>Enviar</button>
+          <button className={styles.button} onClick={handleSubmit}>
+            Send
+          </button>
         </div>
       ) : (
-        <Link href="/login">Login necessário para comentar</Link>
+        <Link href="/login">Login to write a comment</Link>
       )}
       <div className={styles.comments}>
-        <div className={styles.comment}>
-          <div className={styles.user}>
-            <Image
-              src="/p1.jpeg"
-              alt=""
-              width={50}
-              height={50}
-              className={styles.image}
-            />
-            <div className={styles.userInfo}>
-              <span className={styles.username}>Jhon Doe</span>
-              <span className={styles.date}>31/10/2024</span>
-            </div>
-          </div>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            sit illo vero assumenda vitae sint consectetur earum, dolorem vel id
-            itaque cumque voluptas aliquam quia facilis, officia aliquid
-            dignissimos voluptatem.
-          </p>
-        </div>
-      </div>
-      <div className={styles.comments}>
-        <div className={styles.comment}>
-          <div className={styles.user}>
-            <Image
-              src="/p1.jpeg"
-              alt=""
-              width={50}
-              height={50}
-              className={styles.image}
-            />
-            <div className={styles.userInfo}>
-              <span className={styles.username}>Jhon Doe</span>
-              <span className={styles.date}>31/10/2024</span>
-            </div>
-          </div>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            sit illo vero assumenda vitae sint consectetur earum, dolorem vel id
-            itaque cumque voluptas aliquam quia facilis, officia aliquid
-            dignissimos voluptatem.
-          </p>
-        </div>
-      </div>
-      <div className={styles.comments}>
-        <div className={styles.comment}>
-          <div className={styles.user}>
-            <Image
-              src="/p1.jpeg"
-              alt=""
-              width={50}
-              height={50}
-              className={styles.image}
-            />
-            <div className={styles.userInfo}>
-              <span className={styles.username}>Jhon Doe</span>
-              <span className={styles.date}>31/10/2024</span>
-            </div>
-          </div>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            sit illo vero assumenda vitae sint consectetur earum, dolorem vel id
-            itaque cumque voluptas aliquam quia facilis, officia aliquid
-            dignissimos voluptatem.
-          </p>
-        </div>
-      </div>
-      <div className={styles.comments}>
-        <div className={styles.comment}>
-          <div className={styles.user}>
-            <Image
-              src="/p1.jpeg"
-              alt=""
-              width={50}
-              height={50}
-              className={styles.image}
-            />
-            <div className={styles.userInfo}>
-              <span className={styles.username}>Jhon Doe</span>
-              <span className={styles.date}>31/10/2024</span>
-            </div>
-          </div>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            sit illo vero assumenda vitae sint consectetur earum, dolorem vel id
-            itaque cumque voluptas aliquam quia facilis, officia aliquid
-            dignissimos voluptatem.
-          </p>
-        </div>
+        {isLoading
+          ? "loading"
+          : data?.map((item) => (
+              <div className={styles.comment} key={item._id}>
+                <div className={styles.user}>
+                  {item?.user?.image && (
+                    <Image
+                      src={item.user.image}
+                      alt=""
+                      width={50}
+                      height={50}
+                      className={styles.image}
+                    />
+                  )}
+                  <div className={styles.userInfo}>
+                    <span className={styles.username}>{item.user.name}</span>
+                    <span className={styles.date}>{item.createdAt}</span>
+                  </div>
+                </div>
+                <p className={styles.desc}>{item.desc}</p>
+              </div>
+            ))}
       </div>
     </div>
   );
